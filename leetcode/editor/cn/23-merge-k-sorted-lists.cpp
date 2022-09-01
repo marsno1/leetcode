54529ad522f7e0af464fc6178a1f8ee740002a25
madianjun <madianjun@qq.com>
2022-09-01 20:49:51 +0800

    Update

--- leetcode/editor/cn/23-merge-k-sorted-lists.cpp
+++ leetcode/editor/cn/23-merge-k-sorted-lists.cpp
@@ -12,38 +12,124 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-struct compare {
-    bool operator() (ListNode *l, ListNode *r) {
-        return l->val > r->val;
-    }
+
+#define solution_1 true
+#define solution_1_pq true // priority_queue
+#define solution_1_bh false // BinaryHeap
+
+#if solution_1
+// 实现小根堆：定义堆的节点
+struct Node {
+	int key;
+	ListNode *listNode;
+	Node(int key, ListNode *listNode): key(key), listNode(listNode) {}
 };
+#endif
+
+#if solution_1 && solution_1_bh
+// 实现小根堆
+class BinaryHeap {
+public:
+	BinaryHeap() {
+		heap.push_back(Node(0, nullptr)); // 0位置不用
+	}
+
+	bool empty() {
+		return heap.size() == 1;
+	}
+
+	void push(const Node &node) {
+		heap.push_back(node);
+		heapifyUp(heap.size() - 1);
+	}
+
+	Node top() {
+		return heap[1];
+	}
+
+	void pop() {
+		heap[1] = heap[heap.size() - 1];
+		heap.pop_back();
+		heapifyDown(1);
+	}
+
+private:
+	// 向上调整
+	void heapifyUp(int p) {
+		while (p > 1) { // 如果p有父节点，判断是否需要跟父节点交换
+			if (heap[p].key < heap[p/2].key) {
+				swap(heap[p], heap[p/2]);
+				p = p / 2;
+			}
+			else
+				break;
+		}
+	}
+
+	// 向下调整
+	void heapifyDown(int p) {
+		int child = p * 2;
+		while (child < heap.size()) { // 如果子节点存在
+			int other = child + 1; // 另一个子节点
+			if (other < heap.size() && heap[other].key < heap[child].key) {
+				child = other;
+			}
+
+			if (heap[child].key < heap[p].key) {
+				swap(heap[child], heap[p]);
+				p = child; // p下移
+			}
+			else
+				break;
+
+			child = p * 2; // child下移
+		}
+	}
+
+	vector<Node> heap;
+};
+#endif
+
+#if solution_1 && solution_1_pq
+struct compare {
+	bool operator() (Node l, Node r) {
+		return l.key > r.key;
+	}
+};
+#endif
 
 class Solution {
 public:
+#if solution_1
     // 解法一：堆
-//    ListNode* mergeKLists(vector<ListNode*>& lists) {
-//        priority_queue<ListNode*, vector<ListNode*>, compare> q;
-//
-//        for (ListNode *ptr : lists) {
-//            if (ptr)
-//                q.push(ptr);
-//        }
-//
-//        ListNode head;
-//        ListNode *curr = &head;
-//        while(!q.empty()) {
-//            // 取堆顶元素
-//            curr->next = q.top();
-//            q.pop();
-//            curr = curr->next;
-//
-//            // 将对应链表的下一个元素入堆
-//            if (curr->next)
-//                q.push(curr->next);
-//        }
-//        return head.next;
-//    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+#if solution_1_pq
+        priority_queue<Node, vector<Node>, compare> q; // 小根堆
+#elif solution_1_bh
+		BinaryHeap q;
+#endif
+        for (ListNode *ptr : lists) {
+            if (ptr)
+                q.push(Node(ptr->val, ptr));
+        }
 
+        ListNode head;
+        ListNode *curr = &head;
+        while(!q.empty()) {
+            // 取堆顶元素
+            curr->next = q.top().listNode;
+            q.pop();
+            curr = curr->next;
+
+            // 将对应链表的下一个元素入堆
+            if (curr->next)
+                q.push(Node(curr->next->val, curr->next));
+        }
+        return head.next;
+    }
+#endif
+
+#if solution_2
     // 解法二：分治
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
@@ -76,5 +162,6 @@ public:
 
         return head.next;
     }
+#endif
 };
 //leetcode submit region end(Prohibit modification and deletion)
